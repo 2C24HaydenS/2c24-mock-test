daa40c588c24dca7a3a9df0278f2264c0568496e
2C24HaydenS <115450543+2C24HaydenS@users.noreply.github.com>
2022-12-04 12:50:16 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+let num = 0
+input.onButtonPressed(Button.A, function () {
+    if (input.temperature() < 25) {
+        basic.showString("Switch off Air Conditioner")
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    num = 0
+    for (let num = 0; num <= 9; num++) {
+        basic.showNumber(num * 2 + 1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("20211096")
 })
